a4140733b6fd7a0e6db707fbe1bcf32d890f8d26
mikomac405 <56292502+mikomac405@users.noreply.github.com>
2021-08-19 14:38:23 +0200

    Add static tooltips

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from os import pardir
-from ursina import *   
+import numpy as np
+from ursina import *     
 from ursina import shaders
 from ursina.prefabs.first_person_controller import FirstPersonController
+from os import listdir
+from os.path import isfile, join
+
+files = [f for f in listdir('cards') if isfile(join('cards', f))]
 
 app = Ursina()
 
 
-card1_texture = load_texture("assets/crd/c3.jpg")
-card2_texture = load_texture("assets/crd/c4.jpg")
-card3_texture = load_texture("assets/crd/c5.jpg")
+def random_card():
+    pass
 
 class Card(Draggable):
-    def __init__(self, position = (10,15,0), texture = card1_texture):
+    def __init__(self, position = (10,15,0), texture = None):
             super().__init__(
             parent = table,
             position = position,
@@ -21,9 +25,10 @@ class Card(Draggable):
             color = color.white,
             highlight_color = color.lime,
             rotation = (0,180,0),
-            scale = 0.1,
+            scale = 0.07,
             plane_direction = (0,1,0),
-            lock = Vec3(0,1,0)
+            lock = Vec3(0,1,0),
+            tooltip = Tooltip("Onyx\nPrice: 2x diamond, 2x emerald")
         )
 
     # def input(self, key):
@@ -33,7 +38,7 @@ class Card(Draggable):
     #             self.z = mouse.y
 
 class Chip(Draggable):
-    def __init__(self, position = (0.9,1.23,0), texture = None):
+    def __init__(self, position = (0.9,1.23,0), texture = 'assests/chip/chip.tif'):
         super().__init__(
             parent = table,
             position = position,
@@ -45,7 +50,8 @@ class Chip(Draggable):
             rotation = (0,0,0),
             scale = 1,
             plane_direction = (0,1,0),
-            lock = Vec3(0,1,0)
+            lock = Vec3(0,1,0),
+            collision  = True
         )
 
 table = Entity(
@@ -59,12 +65,13 @@ table = Entity(
     shader=shaders.basic_lighting_shader
 )
 
-c1 = Card(position=(1,0.78,0))
+
 
 cube = Entity(
     parent = scene,
     model = 'cube',
     collider = 'box',
+    color = color.gray,
     scale = (30,1,30),
     position = (0,-1,0)
 )
@@ -76,5 +83,10 @@ camera.y = 9
 camera.z = -1.5
 #player = FirstPersonController(y=5, origin_y=-.5)
 
+#c1 = Card(position=(-0.95,0.78,-0.65))
+
+for x in np.arange(-0.975, 1.05, 0.105):
+    print(x)
+    card = Card(position=(x,0.78,-0.6), texture = random.choice(['assets/crd/c3.jpg','assets/crd/c4.jpg','assets/crd/c5.jpg']))
 
 app.run() 
